03889da277571039ad4ca6f5cde788768604094c
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-11-02 13:50:17 -0400

    initial project setup

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import ReactAux from '../ReactAux/ReactAux';
-
+import React, { Component } from "react";
+import ReactAux from "../ReactAux/ReactAux";
+import { MainContent } from "./LayoutElements";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 //HOC, layout is a wrapper technically making it an HOC
 class Layout extends Component {
-    
-    render () {
-        return (
-            <ReactAux>
-                {/*<Toolbar /> */}
-                <main>
-                    {this.props.children}
-                </main>
-                <br />
-                {/*<Footer /> */}
-            </ReactAux>
-        )
-    }
+  render() {
+    return (
+      <ReactAux>
+        <NavBar/>
+        <MainContent>{this.props.children}</MainContent>
+        <br />
+        <Footer />
+      </ReactAux>
+    );
+  }
 }
 
 export default Layout;
---
+++ src/hoc/layout/LayoutElements.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const MainContent = styled.main`
+  margin-top: 32px; /* 56 height of toolbar plus 16 margin top */
+`;
